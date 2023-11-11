e9f7f72193c61a7e147940a146efdd55143ee578
AveePB <mike303.py@gmail.com>
2023-11-11 11:16:39 +0100

    Deleted old project structure

--- README.md
+++
@@ -1,4 +0,0 @@
-# Number-System-Converter
-How to run this program?
- - Double click on number-system-converter.jar file.
- - Use command: java -jar number-system-converter.jar
--- src/Main.java
+++
@@ -1,9 +0,0 @@
-//import app.converter.tests.Tests;
-import app.mainwindow.MainWindow;
-
-public class Main {
-    public static void main(String[] args) {
-        //Tests.run(20);
-        MainWindow mw = new MainWindow();
-    }
-}
