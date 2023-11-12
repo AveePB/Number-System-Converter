d1fb3f34ce1c2bf8a2bd9065042912f666ce2d47
AveePB <mike303.py@gmail.com>
2023-11-12 13:40:46 +0100

    Updated the TF section and the Btn section

--- src/main/java/app/gui/MainWindow.java
+++ src/main/java/app/gui/MainWindow.java
@@ -2,18 +2,20 @@ package app.gui;
 
 //Java Swing
 import javax.swing.*;
+
+//Java Abstract Window Toolkit
 import java.awt.*;
 
 public class MainWindow {
     //Constants:
-    public static final int WINDOW_WIDTH = 370, WINDOW_HEIGHT = 500;
+    public static final int WINDOW_WIDTH = 370, WINDOW_HEIGHT = 250;
     public static final String WINDOW_ICON_PATH = "./assets/img/icon.png";
 
-    //Variables:
+    //GUI components:
     private JButton convertBtn;
     private JComboBox<String> convertFromCB, convertToCB;
     private JFrame mainFrame;
-    private JTextField inputJT, outputJT;
+    private JTextField inputJTF, outputJTF;
 
     /**
      * Constructs a main window object.
@@ -21,8 +23,9 @@ public class MainWindow {
     public MainWindow() {
         initializeMainFrame();
         initializeComboBoxes();
-
-}
+        initializeTextFields();
+        initializeButton();
+    }
 
     /**
      * It creates a totally new JFrame object.
@@ -39,6 +42,10 @@ public class MainWindow {
         this.mainFrame.setIconImage(new ImageIcon(MainWindow.WINDOW_ICON_PATH).getImage());
     }
 
+    /**
+     * It initializes the combo boxes section. It's
+     * required to initialize main frame first!
+     */
     private void initializeComboBoxes() {
         Font titleFont = new Font("", Font.PLAIN, 17);
         Font CBFont = new Font("", Font.BOLD, 13);
@@ -71,6 +78,59 @@ public class MainWindow {
         this.mainFrame.add(this.convertToCB);
     }
 
+    /**
+     * It initializes the text fields section. It's
+     * required to initialize main frame first!
+     */
+    private void initializeTextFields() {
+        Font titleFont = new Font("", Font.PLAIN, 15);
+        Font TFFont = new Font("", Font.PLAIN, 25);
+
+        //Input Text Field
+        JLabel inputLabel = new JLabel("Input");
+        inputLabel.setBounds(30, 70, 130, 40);
+        inputLabel.setFont(titleFont);
+
+        this.inputJTF = new JTextField();
+        this.inputJTF.setBounds(30, 100, 130, 40);
+        this.inputJTF.setFont(TFFont);
+        this.inputJTF.setBorder(BorderFactory.createLineBorder(Color.GRAY));
+
+
+        //Output Text Field
+        JLabel outputLabel = new JLabel("Output");
+        outputLabel.setBounds(200, 70, 130, 40);
+        outputLabel.setFont(titleFont);
+
+        this.outputJTF = new JTextField();
+        this.outputJTF.setBounds(200, 100, 130, 40);
+        this.outputJTF.setFont(TFFont);
+        this.outputJTF.setEditable(false);
+        this.outputJTF.setBorder(BorderFactory.createLineBorder(Color.GRAY));
+
+
+        //Adds GUI components to the frame.
+        this.mainFrame.add(inputLabel);
+        this.mainFrame.add(this.inputJTF);
+        this.mainFrame.add(outputLabel);
+        this.mainFrame.add(this.outputJTF);
+    }
+
+    /**
+     * It initializes the button section. It's
+     * required to initialize main frame first!
+     */
+    private void initializeButton() {
+        Font btnFont = new Font("", Font.PLAIN, 16);
+
+        //Convert Button
+        this.convertBtn = new JButton("Convert");
+        this.convertBtn.setBounds(120, 155, 120, 30);
+        this.convertBtn.setFont(btnFont);
+
+        //Adds GUI components to the frame.
+        this.mainFrame.add(this.convertBtn);
+    }
 
     /**
      * Opens the window.
